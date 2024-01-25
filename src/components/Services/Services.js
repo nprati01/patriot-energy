3c0b84b5663cb25eaa7f1acd4fd5fdb99175c031
Nicole Prati <nicole.e.prati@gmail.com>
2024-01-25 15:17:07 -0500

    Add services page

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -3,8 +3,38 @@ import "./Services.css";
 
 function Services() {
   return (
-    <div>
+    <div className="services-container">
       <h1> Our Services </h1>
+      <div className="service-lists">
+        <div className="service-list1">
+          <ul>
+            <li> General Contracting </li>
+            <li> Earthwork </li>
+            <li> Aggregate </li>
+            <li> Erosion Control </li>
+            <li> Excavation and Reclamation</li>
+            <li> General Contracting </li>
+            <li> Earthwork </li>
+            <li> Aggregate </li>
+            <li> Erosion Control </li>
+            <li> Excavation and Reclamation</li>
+          </ul>
+        </div>
+        <div className="service-list2">
+          <ul>
+            <li> General Contracting </li>
+            <li> Earthwork </li>
+            <li> Aggregate </li>
+            <li> Erosion Control </li>
+            <li> Excavation and Reclamation</li>
+            <li> General Contracting </li>
+            <li> Earthwork </li>
+            <li> Aggregate </li>
+            <li> Erosion Control </li>
+            <li> Excavation and Reclamation</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
