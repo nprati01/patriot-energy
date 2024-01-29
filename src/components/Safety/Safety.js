f5f89bd85f0e8732f367532f14c1356869c962ee
Nicole Prati <nicole.e.prati@gmail.com>
2024-01-29 17:22:58 -0500

    add base functionality for contact, services and safety

--- src/components/Safety/Safety.js
+++ src/components/Safety/Safety.js
@@ -1,10 +1,56 @@
 import React from "react";
+
 import "./Safety.css";
 
 function Safety() {
   return (
-    <div>
-      <h1> Safety </h1>
+    <div className="safety-container">
+      <h1 className="container-title"> Safety </h1>
+
+      <div className="safety-cards">
+        <div className="card">
+          <div className="container-card">
+            <i className="fa-helmet-safety"></i>
+            <p className="card-title">Saftey Card Title 1</p>
+            <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="container-card">
+            <i className="fa-solid fa-helmet-safety"></i>
+            <p className="card-title">Saftey Card Title 2</p>
+            <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="container-card">
+            <i className="fa-solid fa-helmet-safety"></i>
+            <p className="card-title">Saftey Card Title 3</p>
+            <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="container-card">
+            <i className="fa-solid fa-helmet-safety"></i>
+            <p className="card-title">Saftey Card Title 4</p>
+            <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="container-card">
+            <i className="fa-solid fa-helmet-safety"></i>
+            <p className="card-title">Saftey Card Title 5</p>
+            <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="container-card">
+            <i className="fa-solid fa-helmet-safety"></i>
+            <p className="card-title">Saftey Card Title 6</p>
+            <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
