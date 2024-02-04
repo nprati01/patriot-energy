d7be70d4d124957e07a025066e7df283835c6cba
adricastro1 <adricastro1196@gmail.com>
2024-02-04 02:14:33 -0500

    about styling

--- src/components/Safety/Safety.js
+++ src/components/Safety/Safety.js
@@ -11,42 +11,42 @@ function Safety() {
         <div className="card">
         <div className="container-card">
             <i className="fa-solid fa-helmet-safety"></i>
-            <p className="card-title">Saftey Card Title 1</p>
+            <p className="card-title">Safety Card Title 1</p>
             <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
           </div>
         </div>
         <div className="card">
           <div className="container-card">
             <i className="fa-solid fa-helmet-safety"></i>
-            <p className="card-title">Saftey Card Title 2</p>
+            <p className="card-title">Safety Card Title 2</p>
             <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
           </div>
         </div>
         <div className="card">
           <div className="container-card">
             <i className="fa-solid fa-helmet-safety"></i>
-            <p className="card-title">Saftey Card Title 3</p>
+            <p className="card-title">Safety Card Title 3</p>
             <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
           </div>
         </div>
         <div className="card">
           <div className="container-card">
             <i className="fa-solid fa-helmet-safety"></i>
-            <p className="card-title">Saftey Card Title 4</p>
+            <p className="card-title">Safety Card Title 4</p>
             <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
           </div>
         </div>
         <div className="card">
           <div className="container-card">
             <i className="fa-solid fa-helmet-safety"></i>
-            <p className="card-title">Saftey Card Title 5</p>
+            <p className="card-title">Safety Card Title 5</p>
             <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
           </div>
         </div>
         <div className="card">
           <div className="container-card">
             <i className="fa-solid fa-helmet-safety"></i>
-            <p className="card-title">Saftey Card Title 6</p>
+            <p className="card-title">Safety Card Title 6</p>
             <p className="card-description">A fact about the safety features associated with Patriot Energy LLC  100% safety Measures taken </p>
           </div>
         </div>
