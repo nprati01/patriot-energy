f1aab1982641dcde4316018fa0e21857ae8e24b1
Nicole Prati <nicole.e.prati@gmail.com>
2024-02-06 14:37:49 -0500

    add content services page

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -3,10 +3,15 @@ import "./Services.css";
 
 function Services() {
   return (
-    <div className="services-container" id='services'>
+    <div className="services-container" id="services">
       <h1> Our Services </h1>
       <div className="service-lists">
         <div className="service-list1">
+        <h2>Renewable Contstuction</h2>
+          <div className="service-img">
+            <img className="service-img" src="/images/equipment_img.jpg"></img>
+          </div>
+
           <ul>
             <li> General Contracting </li>
             <li> Earthwork </li>
@@ -21,6 +26,49 @@ function Services() {
           </ul>
         </div>
         <div className="service-list2">
+        <h2>Oilfield Services</h2>
+          <div className="service-img">
+            <img className="service-img" src="/images/equipment_img.jpg"></img>
+          </div>
+
+          <ul>
+            <li> General Contracting </li>
+            <li> Earthwork </li>
+            <li> Aggregate </li>
+            <li> Erosion Control </li>
+            <li> Excavation and Reclamation</li>
+            <li> General Contracting </li>
+            <li> Earthwork </li>
+            <li> Aggregate </li>
+            <li> Erosion Control </li>
+            <li> Excavation and Reclamation</li>
+          </ul>
+        </div>
+        <div className="service-list3">
+        <h2>Heavy Civil Construction and Equipment</h2>
+          <div className="service-img">
+            <img className="service-img" src="/images/equipment_img.jpg"></img>
+          </div>
+
+          <ul>
+            <li> General Contracting </li>
+            <li> Earthwork </li>
+            <li> Aggregate </li>
+            <li> Erosion Control </li>
+            <li> Excavation and Reclamation</li>
+            <li> General Contracting </li>
+            <li> Earthwork </li>
+            <li> Aggregate </li>
+            <li> Erosion Control </li>
+            <li> Excavation and Reclamation</li>
+          </ul>
+        </div>
+        <div className="service-list4">
+        <h2>Water Technology and Treatment </h2>
+          <div className="service-img">
+            <img className="service-img" src="/images/equipment_img.jpg"></img>
+          </div>
+
           <ul>
             <li> General Contracting </li>
             <li> Earthwork </li>
